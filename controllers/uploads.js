46e07c7da3b3dcfe5bdcb8d1c56097ee20966910
Daniel Carmona Jaramillo <jcarmod5@jci.com>
2022-03-10 15:58:58 -0500

    Adding UploadFile endpoint

---
+++ controllers/uploads.js
@@ -0,0 +1,24 @@
+
+const { response } = require("express");
+const { subirArchivo } = require("../helpers");
+
+const cargarArchivo = async(req, res = response) => {
+  
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+      res.status(400).json({
+          msg: 'No files were uploaded.'
+      });
+      return;
+    }
+
+    const nombre = await subirArchivo(req.files);
+
+    res.json({
+        nombre
+    })
+    
+};
+
+module.exports = {
+    cargarArchivo
+};
--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../database/config');
 const { auth } = require('google-auth-library');
+const fileUpload = require('express-fileupload');
 
 class Server{
     constructor(){
@@ -13,7 +14,8 @@ class Server{
             usuarios:   '/api/usuarios',
             categorias: '/api/categorias',
             productos:  '/api/productos',
-            buscar:     '/api/buscar'
+            buscar:     '/api/buscar',
+            uploads:     '/api/uploads'
         };
 
         // Connect db
@@ -39,6 +41,12 @@ class Server{
 
         // Public directory
         this.app.use( express.static('public'));
+
+        // Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/'
+        }));
     }
 
     routes(){
@@ -47,6 +55,7 @@ class Server{
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos,  require('../routes/productos'));
         this.app.use(this.paths.buscar,     require('../routes/buscar'));
+        this.app.use(this.paths.uploads,     require('../routes/uploads'));
     }
 
     listen(){
